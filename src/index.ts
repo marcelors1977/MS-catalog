c20c44f80be7ef2ca4e4fd073320d7e930c4761a
marcelors1977 <marcelo.rds1977@gmail.com>
2022-12-17 23:28:09 -0300

    first commit

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import './bootstrap';
+import { MicroCatalogApplication } from './application';
+import { ApplicationConfig } from '@loopback/core';
+
+export { MicroCatalogApplication };
+
+export async function main(options: ApplicationConfig = {}) {
+  const app = new MicroCatalogApplication(options);
+  await app.boot();
+  await app.start();
+
+  const url = app.restServer.url;
+  console.log(`Server is running at ${url}`);
+  console.log(`Try ${url}/ping`);
+
+  return app;
+}
