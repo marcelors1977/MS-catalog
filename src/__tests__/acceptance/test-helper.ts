da86ac22af5ff4e82c83c80adf287b747b187754
marcelors1977 <marcelo.rds1977@gmail.com>
2023-04-05 22:54:58 -0300

    validando CI

--- src/__tests__/acceptance/test-helper.ts
+++ src/__tests__/acceptance/test-helper.ts
@@ -1,9 +1,9 @@
-import {MicroCatalogApplication} from '../..';
-import {givenHttpServerConfig, Client} from '@loopback/testlab';
+import { MicroCatalogApplication } from '../..';
+import { givenHttpServerConfig, Client } from '@loopback/testlab';
 import config from '../../../config';
 import supertest from 'supertest';
 import dbConfig from '../../datasources/esv7.datasource.config';
-import {Esv7DataSource} from '../../datasources';
+import { Esv7DataSource } from '../../datasources';
 
 export async function setupApplication(): Promise<AppWithClient> {
   const restConfig = givenHttpServerConfig({
@@ -19,13 +19,13 @@ export async function setupApplication(): Promise<AppWithClient> {
     rest: restConfig,
   });
 
-  await app.boot();
   app.bind('datasources.esv7').to(testDb);
+  await app.boot();
   await app.start();
 
   const client = supertest('http://127.0.0.1:9000');
 
-  return {app, client};
+  return { app, client };
 }
 
 export const testDb = new Esv7DataSource({
