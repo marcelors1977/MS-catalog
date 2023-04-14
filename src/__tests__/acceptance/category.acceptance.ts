5772d1b3958e6ea75a481843f3ca2037d11e1e01
marcelors1977 <marcelo.rds1977@gmail.com>
2023-04-14 19:03:06 -0300

    ci: :bug: Ajustando erro pipe CI
    Ajustando erro pipe CI

--- src/__tests__/acceptance/category.acceptance.ts
+++ src/__tests__/acceptance/category.acceptance.ts
@@ -1,26 +1,31 @@
 import { Client, expect } from '@loopback/testlab';
 import { MicroCatalogApplication } from '../..';
 import { clearDb, setupApplication } from './test-helper';
+import { appendFile } from 'fs';
 
-describe('Categories', () => {
-  let app: MicroCatalogApplication;
-  let client: Client;
+setTimeout(() => {
+  describe('Categories', () => {
+    let app: MicroCatalogApplication;
+    let client: Client;
 
-  before('setupApplication', async () => {
-    ({ app, client } = await setupApplication());
-  });
+    setTimeout(() => {
+      before('setupApplication', async () => {
+        ({ app, client } = await setupApplication());
+      });
+    }, 5000);
 
-  beforeEach(clearDb);
+    beforeEach(clearDb);
 
-  after(async () => {
-    await app.stop();
-  });
+    after(async () => {
+      await app.stop();
+    });
 
-  it('Invoces GET /categories', async () => {
-    const response = await client.get('/categories').expect(200);
-    expect(response.body).to.containDeep({
-      results: [],
-      count: 0,
+    it('Invoces GET /categories', async () => {
+      const response = await client.get('/categories').expect(200);
+      expect(response.body).to.containDeep({
+        results: [],
+        count: 0,
+      });
     });
   });
-});
+}, 5000);
